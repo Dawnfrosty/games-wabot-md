e8451bcdfb9d349135c26c809cf4b3aa954e2c24
Dawnfrosty <88573246+Dawnfrosty@users.noreply.github.com>
2023-01-29 13:55:15 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6282297563380'],
+  ['6282172353871'],
+  ['6282297563380', 'BG', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.author = 'Olivia Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
