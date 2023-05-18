f682173d870a0eef06dea55637296f594aede86b
Dawnfrosty <88573246+Dawnfrosty@users.noreply.github.com>
2023-05-18 21:17:53 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -153,7 +153,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
     const pp = await conn.profilePictureUrl(conn.user.jid).catch(_ => './src/avatar_contact.png')
-    conn.sendButton(m.chat, text.trim(), author, pp, [
+    conn.sendHydrated(m.chat, text.trim(), author, pp, 'https://github.com/BochilGaming/oliviabot1', 'Github', null, null, [
       ['Donate', '/donasi'],
       ['Speed', '/ping'],
       ['Owner', '/owner']
